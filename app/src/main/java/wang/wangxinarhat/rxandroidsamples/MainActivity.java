125aa3a38b424b88763bce46d21c37b2cde1f5d1
wangxinarhat <wangxinarhat@gmail.com>
2016-04-01 13:57:08 +0800

    add res

--- app/src/main/java/wang/wangxinarhat/rxandroidsamples/MainActivity.java
+++ app/src/main/java/wang/wangxinarhat/rxandroidsamples/MainActivity.java
@@ -3,11 +3,14 @@ package wang.wangxinarhat.rxandroidsamples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
